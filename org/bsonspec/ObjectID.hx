2e4edcbfd8ce7e5adedf72d5d1249b230bcfe089
Jonas Malaco Filho <jonas@jonasmalaco.com>
2014-10-23 19:52:04 -0200

    Notes on current ObjectID implementation

--- org/bsonspec/ObjectID.hx
+++ org/bsonspec/ObjectID.hx
@@ -4,6 +4,7 @@ import haxe.io.Input;
 import haxe.io.Bytes;
 import haxe.io.BytesOutput;
 
+// TODO rename to ObjectId for more consistency with mongoDB docs and Haxe convention
 class ObjectID
 {
 
@@ -12,6 +13,8 @@ class ObjectID
 		if (input == null)
 		{
 			// generate a new id
+            // Maybe: use big endian encoding, like the mongo client (at least) 
+            //        then other clients could use getTimestamp()
 			var out:BytesOutput = new BytesOutput();
 #if haxe3
 			out.writeInt32(Math.floor(Date.now().getTime() / 1000)); // seconds
@@ -37,14 +40,17 @@ class ObjectID
 	}
 
 	public var bytes(default, null):Bytes;
+    // TODO locking or atomic increment
 	private static var sequence:Int = 0;
 
 	// machine host name
 #if (neko || php || cpp)
+    // TODO only first 3 chars, to little; change to hash and/or ip
 	private static var machine:Bytes = Bytes.ofString(sys.net.Host.localhost());
 #else
 	private static var machine:Bytes = Bytes.ofString("flash");
 #end
+    // TODO (sys) on Linux, we can use $PPID, how about on Windows and Mac?
 	private static var pid = Std.random(65536);
 
 }
